64b8c44b89da401e7f13b77621e0510f29df0b07
Thomas Deal <justarandomgeek@gmail.com>
2019-04-10 18:34:33 -0400

    Create two water tiles on the edges so offshore pumps can be placed

--- control.lua
+++ control.lua
@@ -6,8 +6,8 @@ script.on_init(function()
   -- if height wasn't set, just set it... 128 is the default for Ribbon World preset
   mgs.height = mgs.height or 128
 
-  -- ensure height has appropriate margin alignment - needs to be 12 tiles beyond a chunk boundary
-  mgs.height = mgs.height + (24-(mgs.height % 32))
+  -- ensure height has appropriate margin alignment - needs to be 13 tiles beyond a chunk boundary
+  mgs.height = mgs.height + (26-(mgs.height % 32))
 
   surface.map_gen_settings = mgs
 
@@ -37,9 +37,9 @@ function generate_poles(upperlower,surface,area)
     local height = surface.map_gen_settings.height/2
     local poles = {}
     for key,pos in pairs({
-      {area.left_top.x,   upperlower*(height-6)},
-      {area.left_top.x+16,upperlower*(height-6)},
-      {area.left_top.x+32,upperlower*(height-6)},
+      {area.left_top.x,   upperlower*(height-7)},
+      {area.left_top.x+16,upperlower*(height-7)},
+      {area.left_top.x+32,upperlower*(height-7)},
     }) do
       local pole = surface.find_entity("big-electric-pole",pos)
       if not pole then
@@ -80,25 +80,26 @@ script.on_event(defines.events.on_chunk_generated, function(event)
 
       local tiles = {}
       for x=event.area.left_top.x,event.area.right_bottom.x do
-        for y=-(height-1),event.area.right_bottom.y-1 do
+        for y=-(height-2),event.area.right_bottom.y-1 do
           tiles[#tiles+1] = {name=top[x%4] , position = {x,y}}
         end
+        tiles[#tiles+1] = {name='water' , position = {x,-(height-1)}}
         tiles[#tiles+1] = {name='water' , position = {x,-height}}
       end
       surface.set_tiles(tiles)
 
       for x=event.area.left_top.x+1,event.area.right_bottom.x-1,2 do
-        local ent = surface.create_entity{name="express-straight-rail", force=game.forces.neutral, direction=2, position={x,-height+3}}
+        local ent = surface.create_entity{name="express-straight-rail", force=game.forces.neutral, direction=2, position={x,-height+4}}
         ent.minable = false
         ent.destructible = false
 
-        ent = surface.create_entity{name="local-straight-rail", force=game.forces.neutral, direction=2, position={x,-height+9}}
+        ent = surface.create_entity{name="local-straight-rail", force=game.forces.neutral, direction=2, position={x,-height+10}}
         ent.minable = false
         ent.destructible = false
       end
 
-      surface.create_entity{name="rail-signal", force=game.forces.player, direction=2, position={event.area.left_top.x+0.5,-height+1.5}}
-      surface.create_entity{name="rail-signal", force=game.forces.player, direction=2, position={event.area.left_top.x+0.5,-height+7.5}}
+      surface.create_entity{name="rail-signal", force=game.forces.player, direction=2, position={event.area.left_top.x+0.5,-height+2.5}}
+      surface.create_entity{name="rail-signal", force=game.forces.player, direction=2, position={event.area.left_top.x+0.5,-height+8.5}}
 
       generate_poles(-1,surface,event.area)
     elseif event.area.right_bottom.y > height then
@@ -111,25 +112,26 @@ script.on_event(defines.events.on_chunk_generated, function(event)
 
       local tiles = {}
       for x=event.area.left_top.x,event.area.right_bottom.x do
-        for y=event.area.left_top.y,height-2 do
+        for y=event.area.left_top.y,height-3 do
           tiles[#tiles+1] = {name=bottom[x%4] , position = {x,y}}
         end
+        tiles[#tiles+1] = {name='water' , position = {x,height-2}}
         tiles[#tiles+1] = {name='water' , position = {x,height-1}}
       end
       surface.set_tiles(tiles)
 
       for x=event.area.left_top.x+1,event.area.right_bottom.x-1,2 do
-        local ent = surface.create_entity{name="express-straight-rail", force=game.forces.neutral, direction=2, position={x,height-3}}
+        local ent = surface.create_entity{name="express-straight-rail", force=game.forces.neutral, direction=2, position={x,height-4}}
         ent.minable = false
         ent.destructible = false
 
-        ent = surface.create_entity{name="local-straight-rail", force=game.forces.neutral, direction=2, position={x,height-9}}
+        ent = surface.create_entity{name="local-straight-rail", force=game.forces.neutral, direction=2, position={x,height-10}}
         ent.minable = false
         ent.destructible = false
       end
 
-      surface.create_entity{name="rail-signal", force=game.forces.player, direction=6, position={event.area.right_bottom.x-0.5,height-1.5}}
-      surface.create_entity{name="rail-signal", force=game.forces.player, direction=6, position={event.area.right_bottom.x-0.5,height-7.5}}
+      surface.create_entity{name="rail-signal", force=game.forces.player, direction=6, position={event.area.right_bottom.x-0.5,height-2.5}}
+      surface.create_entity{name="rail-signal", force=game.forces.player, direction=6, position={event.area.right_bottom.x-0.5,height-8.5}}
 
       generate_poles(1,surface,event.area)
     end
